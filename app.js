715717ed8a152b9a6e7758674d442a54bfe71fde
Waizul Haque <waiul@gmail.com>
2022-01-02 17:08:01 +0600

    slider

---
+++ app.js
@@ -0,0 +1,22 @@
+const arrows = document.querySelectorAll('.arrow');
+// console.log(arrows);
+const movieLists = document.querySelectorAll('.movie-list');
+// console.log(movieLists);
+
+arrows.forEach((arrow, i) => {
+	const itemNumber = movieLists[i].querySelectorAll('img').length;
+	let clickCounter = 0;
+	arrow.addEventListener('click', () => {
+		clickCounter++;
+		if (itemNumber - (5 + clickCounter) >= 0) {
+			movieLists[i].style.transform = `translateX(${
+				movieLists[i].computedStyleMap().get('transform')[0].x.value -
+				300
+			}px)`;
+		} else {
+			movieLists[i].style.transform = 'translateX(0)';
+			clickCounter = 0;
+		}
+	});
+	console.log(movieLists[i].querySelectorAll('img').length);
+});
